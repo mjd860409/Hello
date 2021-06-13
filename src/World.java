57d89170e192fe0e60ef231f1cd13b99fe575ab8
Manas <mjdas731@gmail.com>
2021-06-13 15:22:35 +0530

    Added one more change

--- src/World.java
+++ src/World.java
@@ -2,7 +2,7 @@ public class World {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Hello World1");
+        System.out.println("Hell World");
 
     }
 }
